453955d5d42cfbe212fea3599e58c79016625a4b
Juan-Dz <juan.diaz33@upr.edu>
2022-12-01 01:50:59 -0400

    Organized and added size as parameter to SnowFlake

--- src/SnowFlake.hpp
+++ src/SnowFlake.hpp
@@ -3,7 +3,6 @@
 #include "ofMain.h"
 #include <cmath>
 #include "AbstractFractal.h"
-#include <string>
 
 /**
  * @brief Koch SnowFlake Fractal
@@ -14,33 +13,34 @@ class SnowFlake : public AbstractFractal {
   private:
     glm::vec2 start;
     glm::vec2 end;
+    float size;
     // string name;
     // int level;
     // int num;
   public:
     SnowFlake();
-    SnowFlake(string name, int level, int num, glm::vec2 start, glm::vec2 end);
-
-    glm::vec2 getStart() const { return start; }
-    glm::vec2 getEnd() const { return end; }
-    void setStart(glm::vec2 start) { this->start = start; }
-    void setEnd(glm::vec2 end) { this->end = end; }
+    SnowFlake(string name, int level, float size, glm::vec2 start, glm::vec2 end);
 
     void draw();
     void draw(int n, SnowFlake *flake);
 
+    //Getters
+    glm::vec2 getStart() const { return start; }
+    glm::vec2 getEnd() const { return end; }
     glm::vec2 getA();
     glm::vec2 getB();
     glm::vec2 getC();
     glm::vec2 getD();
     glm::vec2 getE();
 
-    //Getters
     string getName(){return name;};
     int getLevel(){return level;};
     int getNum(){return num;};
     
     //Setters
+    void setStart(glm::vec2 start) { this->start = start; }
+    void setEnd(glm::vec2 end) { this->end = end; }
+
     void setNum(int num){this->num = num;};
     void setLevel(int level){this->level = level;};
 };
