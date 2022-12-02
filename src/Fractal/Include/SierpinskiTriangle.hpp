95cbf8f019a4cec289c83d778202abeec9196634
Juan-Dz <juan.diaz33@upr.edu>
2022-12-02 17:55:20 -0400

    Began refactering SierpinskiTriangle class

---
+++ src/Fractal/Include/SierpinskiTriangle.hpp
@@ -0,0 +1,16 @@
+// #pragma once
+
+// #include "ofMain.h"
+// #include <cmath>
+// #include "AbstractFractal.hpp"
+
+// class SierpinskiTriangle : public AbstractFractal {
+//   private:
+//     float size;
+//   public:
+//     SierpinskiTriangle():
+//     SierpinskiTriangle(string name; int level, float size);
+
+//     void draw();
+//     void draw(float x, float y, float size, int n);
+// };
