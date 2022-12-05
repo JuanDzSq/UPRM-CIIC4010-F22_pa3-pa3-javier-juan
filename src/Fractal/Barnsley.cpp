c3584d2d81c9f8b7fd1e71b7e3243de06e227cec
Juan-Dz <juan.diaz33@upr.edu>
2022-12-05 19:27:58 -0400

    Barnsley class color change

--- src/Fractal/Barnsley.cpp
+++ src/Fractal/Barnsley.cpp
@@ -41,24 +41,33 @@ void BarnsleyFern::draw(float x, float y, float n) {
     if (n == 0) return;
 
     float r = ofRandom(1);
-
     float px = ofMap(x, -2.1820, 2.6558, 0, ofGetWidth());
     float py = ofMap(y, 0, 9.9983, ofGetHeight(), 0);
 
+    vector <ofColor> color = {ofColor::green, ofColor::blue, ofColor::white};
+    if (n < 6000){
+        ofSetColor(color.at(0));
+    }
+    else if (n < 12000){
+        ofSetColor(color.at(1));
+    }
+    else{
+        ofSetColor(color.at(2));
+    }
+
     ofFill();
-    ofSetColor(ofColor::green);
     ofDrawCircle(px, py, 0.6);
-    ofSetColor(ofColor::white);
 
-    if (r < 0.01)
+    if (r < 0.01){
         draw(0, 0.16 * y, n - 1);
-
-    else if (r < 0.86)
+    }
+    else if (r < 0.86){
         draw(0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y + 1.6, n - 1);
-
-    else if (r < 0.93)
+    }
+    else if (r < 0.93){
         draw(0.2 * x - 0.26 * y, 0.23 * x + 0.22 * y + 1.6, n - 1);
-
-    else
+    }
+    else{
         draw(-0.15 * x + 0.28 * y, 0.26 * x + 0.24 * y + 0.44, n - 1);
+    }
 }
