83e8b45f150f7ef525943f6cd1ae675e28a7a1f3
Juan-Dz <juan.diaz33@upr.edu>
2022-12-05 22:28:53 -0400

    Sierpinski Triangle class color change

--- src/Fractal/SierpinskiTriangle.cpp
+++ src/Fractal/SierpinskiTriangle.cpp
@@ -45,13 +45,23 @@ void SierpinskiTriangle::draw(float x, float y, float size, int n) {
         return;
     }
 
+    if (n <= 1){
+        ofSetColor(237, 295, 12);
+    }
+    else if (n <= 4){
+        ofSetColor(237, 181, 12);
+    }
+    else{
+        ofSetColor(237, 155, 12);
+    }
+
     ofPoint a(x, y);
     ofPoint b(x + size, y);
     ofPoint c(x + size / 2, y + ((sqrt(3) * size) / 2));
-
     ofDrawTriangle(a, b, c);
 
-    draw(x, y, size / 2, n - 1);
+    ofFill();
+    draw(a.x, a.y, size / 2, n - 1);
     draw((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1);
     draw((a.x + c.x) / 2, (((a.y + b.y) / 2) + c.y) / 2, size / 2, n - 1);
 }
